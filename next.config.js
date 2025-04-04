0e193cf933c985c5abf2f7a390d02eec9338dea4
Ho Quoc Hung <hunghq27@gmail.com>
2025-04-04 17:49:25 +0700

    Fix Next.js configuration for static export

--- next.config.js
+++ next.config.js
@@ -1,39 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  reactStrictMode: true,
   images: {
     unoptimized: true,
-    dangerouslyAllowSVG: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-    domains: ['localhost'],
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(jpg|jpeg|png|gif|svg)$/i,
-      type: 'asset/resource',
-    });
-    return config;
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'hoquochung.cloud',
-          },
-        ],
-        destination: 'https://www.hoquochung.cloud',
-        permanent: true,
-      },
-    ]
   },
 };
 
